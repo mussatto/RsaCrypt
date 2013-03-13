9ed81214cbfc70651bb935e2d1a05aa6fbd1f88a
Vitor Mussatto <vitor.mussatto@novapontocom.com.br>
2013-03-13 10:57:30 -0300

    renaming file

---
+++ src/cli/cryptXml.java
@@ -0,0 +1,25 @@
+package cli;
+
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author vitormussatto
+ */
+public class cryptXml {
+    
+    public static void main(String [] args){
+        try {
+                String xml = "<?xml version='1.0' encoding='utf-8'?><ORDERINFORMATION>  <ORDER>    <BRAND_ID>2</BRAND_ID></ORDER></ORDERINFORMATION>";
+
+                RSAHelper rsahelper = new RSAHelper();
+                String encripted = rsahelper.encrypt(xml);
+                //Do something
+            } catch (Exception ex) {
+            Logger.getLogger(criptXml.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
